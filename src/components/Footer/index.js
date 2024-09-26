20edc4be16be48182b762f42f10c8f2373a34102
BryanAlmeida016 <bryalmeida.moraes06@gmail.com>
2024-09-26 11:37:38 -0300

    Arrumando

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2> EtecFlix &copy; por Bryan Almeida</h2>
+        </footer>
+    );
+}
+
+export default Footer;
